ecad86e650a4ef26b6fd1bc6ee16406c915ba14f
enesaltunoglu <enes@veriyaz.com.tr>
2020-11-13 04:25:55 +0300

    color change on tap and calculating board size added

--- lib/components/dots.dart
+++ lib/components/dots.dart
@@ -1,21 +1,42 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
 import 'package:flutter_app/nokta_game.dart';
 
 class Dot {
-  NoktaGame game;
+  final NoktaGame game;
   Rect dotRect;
+  RRect dotRectx;
   Paint dotPaint;
+  bool isDead = false;
+  bool isOffScreen = false;
 
-  Dot(game, double x, double y) {
-    dotRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+  Dot(this.game, double x, double y) {
+    // dotRect = Rect.fromLTWH(x, y, game.tileSize, game.tileSize);
+    dotRect = Rect.fromCircle(center: Offset(100, 100), radius: 10);
+    dotRectx = RRect.fromRectXY(dotRect, 1000, 1000);
     dotPaint = Paint();
-    dotPaint.color = Color(0xff6ab04c);
+    dotPaint.color = Colors.red;
   }
 
   void render(Canvas c) {
-    c.drawRect(dotRect, dotPaint);
+    // c.drawRect(dotRect, dotPaint);
+    c.drawRRect(dotRectx, dotPaint);
+    // c.drawCircle(Offset(200, 200), 100, dotPaint);
+    // c.drawCircle(Offset(200, 500), 100, dotPaint);
   }
 
-  void update(double t) {}
+  void update(double t) {
+    // if (isDead) {
+    //   dotRect = dotRect.translate(0, game.tileSize * 12 * t);
+    // }
+    // if (dotRect.top > game.screenSize.height) {
+    //   isOffScreen = true;
+    // }
+  }
 
+  void onTapDown() {
+    // isDead = true;
+    dotPaint.color = Colors.black;
+    // game.spawnDot();
+  }
 }
--- lib/nokta_game.dart
+++ lib/nokta_game.dart
@@ -1,15 +1,21 @@
-
 import 'dart:ui';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter_app/components/dots.dart';
 
+double boardWidth;
+double boardHeight;
+double widthMargin;
+double heightMargin;
+
 class NoktaGame extends Game {
   Size screenSize;
+  Size boardSize;
   double tileSize;
   List<Dot> dots;
-  bool hasWon;
 
   NoktaGame() {
     initialize();
@@ -17,56 +23,67 @@ class NoktaGame extends Game {
 
   void initialize() async {
     dots = List<Dot>();
+    // rnd = Random();
     resize(await Flame.util.initialDimensions());
-    // en başta şeffaf noktalar construct edip sonrasında, spawnDot yerine colorDot methodunu çağırabiliriz.
     spawnDot();
   }
 
-  void render(Canvas canvas) {
-
-    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint bgPaint = Paint();
-    bgPaint.color = Colors.white70;
-    canvas.drawRect(bgRect, bgPaint);
-
-    Paint linePaint = Paint();
-    linePaint.color = Colors.blueAccent;
-    for (var i = 0; i < 500; i=i+50) {
-      canvas.drawLine(Offset(i.toDouble(),0), Offset(i.toDouble(),1000), linePaint);
-    }
-    for (var j = 0; j < 1500; j=j+50) {
-      canvas.drawLine(Offset(0,j.toDouble()), Offset(1000,j.toDouble()), linePaint);
-    }
-
-    dots.forEach((Dot dot) => dot.render(canvas));
-
-  }
-
   void spawnDot() {
+    // double x = rnd.nextDouble() * (screenSize.width - tileSize);
+    // double y = rnd.nextDouble() * (screenSize.height - tileSize);
     dots.add(Dot(this, 50, 50));
   }
 
-  void onTapDown(TapDownDetails d) {
-    double screenCenterX = screenSize.width / 2;
-    double screenCenterY = screenSize.height / 2;
-    if (d.globalPosition.dx >= screenCenterX - 75
-        && d.globalPosition.dx <= screenCenterX + 75
-        && d.globalPosition.dy >= screenCenterY - 75
-        && d.globalPosition.dy <= screenCenterY + 75
-    ) {
-      hasWon = true;
+  void render(Canvas canvas) {
+    boardRendering(canvas);
+
+    Paint linePaint = Paint();
+    linePaint.color = Colors.blueAccent;
+    for (var i = 0; i < 500; i = i + 50) {
+      canvas.drawLine(
+          Offset(i.toDouble(), 0), Offset(i.toDouble(), 1000), linePaint);
     }
+    for (var j = 0; j < 1500; j = j + 50) {
+      canvas.drawLine(
+          Offset(0, j.toDouble()), Offset(1000, j.toDouble()), linePaint);
+    }
+
+    dots.forEach((Dot dot) => dot.render(canvas));
   }
 
   void update(double t) {
-    // TODO: implement update
     dots.forEach((Dot dot) => dot.update(t));
+    // dots.removeWhere((Dot dot) => dot.isOffScreen);
   }
 
   void resize(Size size) {
     screenSize = size;
-    super.resize(size);
     tileSize = screenSize.width / 9;
+
+    calculateBoardSize(screenSize);
   }
 
+  void onTapDown(TapDownDetails d) {
+    dots.forEach((Dot dot) {
+      if (dot.dotRect.contains(d.globalPosition)) {
+        dot.onTapDown();
+      }
+    });
+  }
+}
+
+boardRendering(Canvas canvas) {
+  Rect board =
+      Rect.fromLTWH(widthMargin / 2, heightMargin / 2, boardWidth, boardHeight);
+  Paint boardColor = Paint();
+  boardColor.color = Colors.white70;
+  canvas.drawRect(board, boardColor);
+}
+
+calculateBoardSize(Size screenSize) {
+  widthMargin = screenSize.width % 50;
+  boardWidth = screenSize.width - widthMargin;
+
+  heightMargin = screenSize.height % 50;
+  boardHeight = screenSize.height - heightMargin;
 }
